315b8d507aa7b595e26d34f93d234f416f3ab79a
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-11-14 19:06:17 -0500

    Deployable

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000'
         break;
-    case 'playlister-server-api.herokuapp.com':
+    case 'playlister-client.herokuapp.com':
         APIURL = 'https://playlister-server-api.herokuapp.com'
 }
 
